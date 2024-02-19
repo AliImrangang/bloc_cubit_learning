6c33182d0a84a94ceba650d1864189cbec47f309
AliImrangang <aliimranm2242@gmail.com>
2024-02-19 13:59:32 +0400

    Initial commit

---
+++ lib/Views/HomePage.dart
@@ -0,0 +1,49 @@
+import 'package:bloc_cubit_learning/blocs/Internet_bloc/internet_bloc.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+        child: Scaffold(
+      body: Center(
+        child: BlocConsumer<InternetBloc, InternetState>(
+            builder: (context, state) {
+          if (state is InternetGainedState) {
+            return Text("Connected!");
+          } else if (state is InternetLostState) {
+            return Text("Internet Lost");
+          } else {
+            return Text("Loading ....");
+          }
+          // return Text("Loading");
+        }, listener: (context, state) {
+          if (state is InternetGainedState) {
+            ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+              content: Text("Internet Connected"),
+              backgroundColor: Colors.green,
+            ));
+          } else if (state is InternetLostState) {
+            ScaffoldMessenger.of(context)
+                .showSnackBar(SnackBar(content: Text("Internet Lost..")));
+          }
+        }),
+        // child: BlocBuilder<InternetBloc, InternetState>(
+        //   builder: (context, state) {
+        //     if (state is InternetGainedState) {
+        //       return Text("Connected!");
+        //     } else if (state is InternetLostState) {
+        //       return Text("Internet Lost");
+        //     } else {
+        //       return Text("Loading ....");
+        //     }
+        //     // return Text("Loading");
+        //   },
+        // ),
+      ),
+    ));
+  }
+}
